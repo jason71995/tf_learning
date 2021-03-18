1cad576383fb8f4494936e2f880ac8320a0b09f0
jason71995 <jason71995@gmail.com>
2021-03-19 02:54:29 +0800

    Add MNIST LeNet5 example.

--- lib/layers.py
+++ lib/layers.py
@@ -15,3 +15,22 @@ class Dense(tf.Module):
 
     def __call__(self, inputs):
         return tf.matmul(inputs, self.weight) + self.bias
+
+
+class Conv2D(tf.Module):
+    def __init__(self, k_size, in_dim, out_dim, strides=(1,1), padding="SAME", dilations=None, name=None):
+        super(Conv2D, self).__init__(name)
+        self.strides = strides
+        self.padding = padding
+        self.dilations = dilations
+        self.weight = tf.Variable(
+            initial_value=tf.random.normal(k_size + (in_dim, out_dim), 0.0, tf.sqrt(2.0/(in_dim+out_dim))),
+            trainable=True,
+            name="{}_weight".format(name))
+        self.bias = tf.Variable(
+            initial_value=tf.zeros((out_dim, )),
+            trainable=True,
+            name="{}_bias".format(name))
+
+    def __call__(self, inputs):
+        return tf.nn.conv2d(inputs, self.weight,strides=self.strides,padding=self.padding,dilations=self.dilations) + self.bias
--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from lib.layers import Dense
+from lib.layers import Dense, Conv2D
 
 
 class LeNet_300_100(tf.Module):
@@ -7,10 +7,41 @@ class LeNet_300_100(tf.Module):
         super(LeNet_300_100, self).__init__(name)
         self.dense1 = Dense(input_dim,300,"dense1")
         self.dense2 = Dense(300,100,"dense2")
-        self.classifier = Dense(100,output_dim,"dense2")
+        self.classifier = Dense(100,output_dim,"classifier")
 
     def __call__(self, inputs):
         y = self.dense1(inputs)
+        y = tf.nn.relu(y)
+        y = self.dense2(y)
+        y = tf.nn.relu(y)
+        y = self.classifier(y)
+        y = tf.nn.softmax(y,axis=-1)
+        return y
+
+
+class LeNet5(tf.Module):
+    def __init__(self, image_shape, num_classes, name="lenet5"):
+        super(LeNet5, self).__init__(name)
+        height, width, channel = image_shape
+        self.conv1 = Conv2D((3,3), channel, 6, padding="SAME", name="conv1")
+        self.conv2 = Conv2D((3,3), 6, 16, padding="SAME", name="conv2")
+        self.dense1 = Dense(16*(height//4)*(width//4), 120, "dense1")
+        self.dense2 = Dense(120, 84, "dense2")
+        self.classifier = Dense(84, num_classes, "classifier")
+
+    def __call__(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+
+        y = self.conv1(inputs)
+        y = tf.nn.relu(y)
+        y = tf.nn.max_pool(y,(2,2),(2,2),"VALID")
+
+        y = self.conv2(y)
+        y = tf.nn.relu(y)
+        y = tf.nn.max_pool(y,(2,2),(2,2),"VALID")
+
+        y = tf.reshape(y, (batch_size, -1))
+        y = self.dense1(y)
         y = tf.nn.relu(y)
         y = self.dense2(y)
         y = tf.nn.relu(y)
