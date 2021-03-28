789fcfc207b112e39d191f058b3313cf75c149e7
jason71995 <jason71995@gmail.com>
2021-03-28 16:17:07 +0800

    Add ResNet50

--- lib/layers.py
+++ lib/layers.py
@@ -34,3 +34,55 @@ class Conv2D(tf.Module):
 
     def __call__(self, inputs):
         return tf.nn.conv2d(inputs, self.weight,strides=self.strides,padding=self.padding,dilations=self.dilations) + self.bias
+
+
+class BatchNormalization(tf.Module):
+
+    def __init__(self, feature_dim, momentum=0.9, name=None):
+        super(BatchNormalization, self).__init__(name)
+        self.momentum = momentum
+
+        self.std = tf.Variable(
+            initial_value=tf.ones((feature_dim,)),
+            trainable=False,
+            name="{}_std".format(name))
+        self.mean = tf.Variable(
+            initial_value=tf.zeros((feature_dim,)),
+            trainable=False,
+            name="{}_mean".format(name))
+        self.gamma = tf.Variable(
+            initial_value=tf.ones((feature_dim,)),
+            trainable=True,
+            name="{}_gamma".format(name))
+        self.beta = tf.Variable(
+            initial_value=tf.zeros((feature_dim,)),
+            trainable=True,
+            name="{}_beta".format(name))
+
+    def __call__(self, inputs, training):
+
+        if training:
+            in_rank = tf.rank(inputs)
+            axis = tf.range(in_rank)[:-1]
+            in_mean = tf.reduce_mean(inputs, axis)
+            in_std = tf.math.reduce_std(inputs, axis)
+            self.mean.assign((1.0-self.momentum)*in_mean+self.momentum*self.mean)
+            self.std.assign((1.0-self.momentum)*in_std+self.momentum*self.std)
+
+        y = (inputs-self.mean)/(self.std+1e-7)
+        y = self.gamma*y+self.beta
+        return y
+
+
+class Dropout(tf.Module):
+    def __init__(self, drop_rate, name=None):
+        super(Dropout, self).__init__(name)
+        self.drop_rate = drop_rate
+
+    def __call__(self, inputs, training):
+        if training:
+            # return tf.nn.dropout(inputs, self.rate)
+            mask = tf.cast(tf.random.uniform((tf.shape(inputs)[-1], ), 0.0, 1.0) > self.drop_rate, "float32")
+            return mask * inputs / (1.0-self.drop_rate)
+
+        return inputs
---
+++ lib/models/resnet.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+from lib.layers import Dense, Conv2D, BatchNormalization
+
+
+class ResNet50(tf.Module):
+    def __init__(self, image_shape, num_classes, name="resnet50"):
+        super(ResNet50, self).__init__(name)
+
+        self.conv1 = Conv2D(k_size=(7, 7), in_dim=image_shape[-1], out_dim=64, strides=(2,2), name="conv1")
+        self.bn1 = BatchNormalization(64, name="bn1")
+
+        self.blocks = [
+            ResBlock(64,  256, (1, 1), "s1_b1"),
+            ResBlock(256, 256, (1, 1), "s1_b2"),
+            ResBlock(256, 256, (1, 1), "s1_b3"),
+
+            ResBlock(256, 512, (2, 2), "s2_b1"),
+            ResBlock(512, 512, (1, 1), "s2_b2"),
+            ResBlock(512, 512, (1, 1), "s2_b3"),
+            ResBlock(512, 512, (1, 1), "s2_b4"),
+
+            ResBlock(512,  1024, (2, 2), "s3_b1"),
+            ResBlock(1024, 1024, (1, 1), "s3_b2"),
+            ResBlock(1024, 1024, (1, 1), "s3_b3"),
+            ResBlock(1024, 1024, (1, 1), "s3_b4"),
+            ResBlock(1024, 1024, (1, 1), "s3_b5"),
+            ResBlock(1024, 1024, (1, 1), "s3_b6"),
+
+            ResBlock(1024, 2048, (2, 2), "s4_b1"),
+            ResBlock(2048, 2048, (1, 1), "s4_b2"),
+            ResBlock(2048, 2048, (1, 1), "s4_b3"),
+        ]
+
+        self.dense = Dense(2048, num_classes, "dense3")
+
+    def __call__(self, inputs, training):
+        y = self.conv1(inputs)
+        y = self.bn1(y, training)
+        y = tf.nn.relu(y)
+        y = tf.nn.max_pool(y, (3, 3), (2, 2), "SAME")
+
+        for block in self.blocks:
+            y = block(y, training)
+
+        # global pooling
+        y = tf.reduce_mean(y, axis=[1,2])
+        y = self.dense(y)
+        y = tf.nn.softmax(y,axis=-1)
+        return y
+
+
+class ResBlock(tf.Module):
+    def __init__(self, in_filters, out_filters, strides, name):
+        super(ResBlock, self).__init__(name)
+
+        self.is_short_cut = in_filters != out_filters
+        if self.is_short_cut:
+            self.conv0 = Conv2D(k_size=(1, 1), in_dim=in_filters, out_dim=out_filters, strides=strides, name="{}_conv0".format(name))
+            self.bn0 = BatchNormalization(out_filters,name="{}_bn0".format(name))
+
+        self.conv1 = Conv2D(k_size=(1, 1), in_dim=in_filters, out_dim=out_filters//4, strides=strides, name="{}_conv1".format(name))
+        self.bn1 = BatchNormalization(out_filters//4,name="{}_bn1".format(name))
+        self.conv2 = Conv2D(k_size=(3, 3), in_dim=out_filters//4, out_dim=out_filters//4, name="{}_conv2".format(name))
+        self.bn2 = BatchNormalization(out_filters//4,name="{}_bn2".format(name))
+        self.conv3 = Conv2D(k_size=(1, 1), in_dim=out_filters//4, out_dim=out_filters, name="{}_conv3".format(name))
+        self.bn3 = BatchNormalization(out_filters,name="{}_bn3".format(name))
+
+    def __call__(self, inputs, training):
+
+        if self.is_short_cut:
+            short_cut = self.conv0(inputs)
+            short_cut = self.bn0(short_cut, training)
+        else:
+            short_cut = inputs
+
+        y = self.conv1(inputs)
+        y = self.bn1(y, training)
+        y = tf.nn.relu(y)
+
+        y = self.conv2(y)
+        y = self.bn2(y, training)
+        y = tf.nn.relu(y)
+
+        y = self.conv3(y)
+        y = self.bn3(y, training)
+        y = y + short_cut
+        y = tf.nn.relu(y)
+        return y
